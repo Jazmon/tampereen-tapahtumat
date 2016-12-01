33d9499329fb7a34cf5e7157ff7bbb7ff0057926
Atte Huhtakangas <atte.huhtakangas@gmail.com>
2016-12-01 20:12:04 +0200

    enable custom map style

--- src/App.js
+++ src/App.js
@@ -39,6 +39,7 @@ import {
   DARK_PRIMARY_COLOR,
 } from './theme';
 
+import mapStyle from './mapStyle.json';
 import Marker from './components/Marker';
 // import Base from './components/Base';
 import Loading from './components/Loading';
@@ -46,7 +47,7 @@ import BottomSheet from './components/BottomSheet';
 import Slider from './components/Slider';
 import FloatingActionButton from './components/FloatingActionButton';
 
-const duration = 120;
+const DURATION = 120;
 
 const { Calendar } = NativeModules;
 
@@ -274,222 +275,6 @@ class App extends Component {
     const events: Array<Event> = getCurrentEvents(this.state.events, this.state.date);
     const currentMarkers: Array<MapMarker> = eventsToMarkers(events);
 
-
-    const customMapStyle = [
-      {
-        elementType: 'geometry',
-        stylers: [
-          {
-            color: '#ebe3cd',
-          },
-        ],
-      },
-      {
-        elementType: 'labels.text.fill',
-        stylers: [
-          {
-            color: '#523735',
-          },
-        ],
-      },
-      {
-        elementType: 'labels.text.stroke',
-        stylers: [
-          {
-            color: '#f5f1e6',
-          },
-        ],
-      },
-      {
-        featureType: 'administrative',
-        elementType: 'geometry.stroke',
-        stylers: [
-          {
-            color: '#c9b2a6',
-          },
-        ],
-      },
-      {
-        featureType: 'administrative.land_parcel',
-        elementType: 'geometry.stroke',
-        stylers: [
-          {
-            color: '#dcd2be',
-          },
-        ],
-      },
-      {
-        featureType: 'administrative.land_parcel',
-        elementType: 'labels.text.fill',
-        stylers: [
-          {
-            color: '#ae9e90',
-          },
-        ],
-      },
-      {
-        featureType: 'landscape.natural',
-        elementType: 'geometry',
-        stylers: [
-          {
-            color: '#dfd2ae',
-          },
-        ],
-      },
-      {
-        featureType: 'poi',
-        elementType: 'geometry',
-        stylers: [
-          {
-            color: '#dfd2ae',
-          },
-        ],
-      },
-      {
-        featureType: 'poi',
-        elementType: 'labels.text.fill',
-        stylers: [
-          {
-            color: '#93817c',
-          },
-        ],
-      },
-      {
-        featureType: 'poi.park',
-        elementType: 'geometry.fill',
-        stylers: [
-          {
-            color: '#a5b076',
-          },
-        ],
-      },
-      {
-        featureType: 'poi.park',
-        elementType: 'labels.text.fill',
-        stylers: [
-          {
-            color: '#447530',
-          },
-        ],
-      },
-      {
-        featureType: 'road',
-        elementType: 'geometry',
-        stylers: [
-          {
-            color: '#f5f1e6',
-          },
-        ],
-      },
-      {
-        featureType: 'road.arterial',
-        elementType: 'geometry',
-        stylers: [
-          {
-            color: '#fdfcf8',
-          },
-        ],
-      },
-      {
-        featureType: 'road.highway',
-        elementType: 'geometry',
-        stylers: [
-          {
-            color: '#f8c967',
-          },
-        ],
-      },
-      {
-        featureType: 'road.highway',
-        elementType: 'geometry.stroke',
-        stylers: [
-          {
-            color: '#e9bc62',
-          },
-        ],
-      },
-      {
-        featureType: 'road.highway.controlled_access',
-        elementType: 'geometry',
-        stylers: [
-          {
-            color: '#e98d58',
-          },
-        ],
-      },
-      {
-        featureType: 'road.highway.controlled_access',
-        elementType: 'geometry.stroke',
-        stylers: [
-          {
-            color: '#db8555',
-          },
-        ],
-      },
-      {
-        featureType: 'road.local',
-        elementType: 'labels.text.fill',
-        stylers: [
-          {
-            color: '#806b63',
-          },
-        ],
-      },
-      {
-        featureType: 'transit.line',
-        elementType: 'geometry',
-        stylers: [
-          {
-            color: '#dfd2ae',
-          },
-        ],
-      },
-      {
-        featureType: 'transit.line',
-        elementType: 'labels.text.fill',
-        stylers: [
-          {
-            color: '#8f7d77',
-          },
-        ],
-      },
-      {
-        featureType: 'transit.line',
-        elementType: 'labels.text.stroke',
-        stylers: [
-          {
-            color: '#ebe3cd',
-          },
-        ],
-      },
-      {
-        featureType: 'transit.station',
-        elementType: 'geometry',
-        stylers: [
-          {
-            color: '#dfd2ae',
-          },
-        ],
-      },
-      {
-        featureType: 'water',
-        elementType: 'geometry.fill',
-        stylers: [
-          {
-            color: '#b9d3c2',
-          },
-        ],
-      },
-      {
-        featureType: 'water',
-        elementType: 'labels.text.fill',
-        stylers: [
-          {
-            color: '#92998d',
-          },
-        ],
-      },
-    ];
     return (
       <View style={styles.mapContainer}>
         <MapView
@@ -504,7 +289,7 @@ class App extends Component {
           showsUserLocation={true}
           showsMyLocationButton={false}
           showsCompass={false}
-          customMapStyle={customMapStyle}
+          customMapStyle={mapStyle}
           toolbarEnabled={true}
           // provider="google"
           // onRegionChange={this.onRegionChange}
@@ -529,7 +314,7 @@ class App extends Component {
     } = this.state;
 
     Animated.timing(bottomSheetColorAnimated, {
-      duration,
+      duration: DURATION,
       toValue: bottomSheetColor,
     }).start();
 

411272c24afcc880cf6083deeefc2356b22eb8b3
Atte Huhtakangas <atte.huhtakangas@gmail.com>
2016-12-01 19:41:54 +0200

    upgrade react-native version to 0.38

--- android/app/src/main/java/com/tampereentapahtumat/MainApplication.java
+++ android/app/src/main/java/com/tampereentapahtumat/MainApplication.java
@@ -16,6 +16,7 @@ import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
 
 import java.util.Arrays;
 import java.util.List;
@@ -49,4 +50,10 @@ public class MainApplication extends Application implements ReactApplication {
     public ReactNativeHost getReactNativeHost() {
         return mReactNativeHost;
     }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+  }
 }

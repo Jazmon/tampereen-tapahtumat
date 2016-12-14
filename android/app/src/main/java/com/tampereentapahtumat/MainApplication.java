f92b0c1b9ee9454717fcadd4dc500a1e93ed3f38
Atte Huhtakangas <atte.huhtakangas@gmail.com>
2016-12-14 21:52:21 +0200

    add new relic monitoring

--- android/app/src/main/java/com/tampereentapahtumat/MainApplication.java
+++ android/app/src/main/java/com/tampereentapahtumat/MainApplication.java
@@ -1,21 +1,21 @@
 package com.tampereentapahtumat;
 
 import android.app.Application;
-import android.util.Log;
 
-import com.facebook.react.ReactApplication;
-import com.react.rnspinkit.RNSpinkitPackage;
-import com.oblador.vectoricons.VectorIconsPackage;
 import com.airbnb.android.react.maps.MapsPackage;
-import com.i18n.reactnativei18n.ReactNativeI18n;
-import com.devfd.RNGeocoder.RNGeocoderPackage;
+import com.attehuhtakangas.navigationbar.NavigationBarPackage;
 import com.bottomsheetbehavior.BottomSheetBehaviorPackage;
 import com.cmcewen.blurview.BlurViewPackage;
-import com.attehuhtakangas.navigationbar.NavigationBarPackage;
-import com.facebook.react.ReactInstanceManager;
+import com.devfd.RNGeocoder.RNGeocoderPackage;
+import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
+import com.i18n.reactnativei18n.ReactNativeI18n;
+import com.newrelic.agent.android.NewRelic;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.react.rnspinkit.RNSpinkitPackage;
+import com.wix.rnnewrelic.RNNewRelicPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -40,7 +40,8 @@ public class MainApplication extends Application implements ReactApplication {
                     new BlurViewPackage(),
                     new NavigationBarPackage(),
                     new BottomSheetBehaviorPackage(),
-                    new RNCalendarPackage()
+                    new RNCalendarPackage(),
+                    new RNNewRelicPackage()
             );
         }
     };
@@ -48,5 +49,10 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     public ReactNativeHost getReactNativeHost() {
         return mReactNativeHost;
+    }
+
+    public void onCreate() {
+        super.onCreate();
+        NewRelic.withApplicationToken(BuildConfig.NEW_RELIC_TOKEN).start(this);
     }
 }

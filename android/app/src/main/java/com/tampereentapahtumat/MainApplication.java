35af04a3f2c9ae9d5d2dbdab57bdb6d4aa717ce6
Atte Huhtakangas <atte.huhtakangas@gmail.com>
2018-05-10 19:35:12 +0300

    Run rn git upgrade

--- android/app/src/main/java/com/tampereentapahtumat/MainApplication.java
+++ android/app/src/main/java/com/tampereentapahtumat/MainApplication.java
@@ -11,17 +11,22 @@ import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
+<<<<<<< ours
 import com.i18n.reactnativei18n.ReactNativeI18n;
 import com.newrelic.agent.android.NewRelic;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.react.rnspinkit.RNSpinkitPackage;
 import com.wix.rnnewrelic.RNNewRelicPackage;
+=======
+import com.facebook.soloader.SoLoader;
+>>>>>>> theirs
 
 import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
 
+<<<<<<< ours
     private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
         @Override
         protected boolean getUseDeveloperSupport() {
@@ -45,14 +50,41 @@ public class MainApplication extends Application implements ReactApplication {
             );
         }
     };
+=======
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    @Override
+    public boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
+>>>>>>> theirs
 
     @Override
     public ReactNativeHost getReactNativeHost() {
         return mReactNativeHost;
     }
+<<<<<<< ours
 
     public void onCreate() {
         super.onCreate();
         NewRelic.withApplicationToken(BuildConfig.NEW_RELIC_TOKEN).start(this);
     }
+=======
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
+    }
+  };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+  }
+>>>>>>> theirs
 }
